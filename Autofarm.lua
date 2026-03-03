774dda8c1ca0ee78e34a2e7adaa5fe04d39d14dd
EzkieMalia <ezekielquisky1z@outlook.com>
2026-03-02 17:14:44 -0700

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -5,6 +5,8 @@ if game.PlaceId == 15124180230 then
 end
 queue_on_teleport([[
     repeat task.wait() until game:IsLoaded()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/helloautofarm/refs/heads/main/RealAutofarm.lua"))()
+    if game.PlaceId ~= 15124180230 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/helloautofarm/refs/heads/main/Autoexecute.lua"))()
+        end
 ]])
 end)
