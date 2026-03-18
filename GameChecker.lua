84be0ffecb6655220bb100b0ea1ca06556610306
Logan <ezekielquisky1z@outlook.com>
2026-03-17 19:19:32 -0700

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,5 +1,5 @@
 queue_on_teleport([[
-if game.PlaceId ~= then
+if game.PlaceId ~= 15124180230 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/Autofarm/refs/heads/main/Teleporter.lua"))()
 else
   loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/Autofarm/refs/heads/main/Autofarm.lua"))()
